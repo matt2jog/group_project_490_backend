1deebd2a8a76c76bca9abfbb2d1fe2afc84021a2
Rodri09890 <129125043+Rodri09890@users.noreply.github.com>
2026-03-11 22:35:26 -0400

    Update client models.py

--- src/database/client/models.py
+++ src/database/client/models.py
@@ -1,18 +1,6 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime, date
 
-class PaymentInformation(SQLModel, table=True):
-  __tablename__ = "payment_information"
-  id : int = Field(primary_key=True)
-  ccnum : str
-  cv : str
-  exp_date : date
-  last_updated : datetime
-
-class ClientAvailability(SQLModel, table=True):
-  id : int = Field(primary_key=True)
-  id_weekly : bool
-
 class Client(SQLModel, table=True):
   __tablename__ = "client"
   id: int = Field(primary_key=True)
@@ -20,4 +8,22 @@ class Client(SQLModel, table=True):
   client_availability_id : int = Field(foreign_key="client_availability.id")
   last_update : datetime
 
+class ClientAvailability(SQLModel, table=True):
+  __tablename__ = "client_availability"
+  id : int = Field(primary_key=True)
+  id_weekly : bool
 
+class FitnessGoals(SQLModel, table=True):
+  __tablename__ = "fitness_goals"
+  id : int = Field(primary_key=True)
+  client_id : int = Field(foreign_key="client.id")
+  goal_enum : str
+  last_updated : datetime
+
+class ClientWorkoutPlan(SQLModel, table=True):
+  __tablename__ = "client_workout_plan"
+  id: int = Field(primary_key=True)
+  client_id : int = Field(foreign_key="client.id")
+  workout_plan_id : int = Field(foreign_key="workout_plan.id")
+  start_time : datetime
+  end_time : datetime
